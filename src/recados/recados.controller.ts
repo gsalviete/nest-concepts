89f1a018e5311a9c485ffaab936100ffaacd6f81
Gabriel Salviete <gsalviete@gmail.com>
2025-11-25 15:19:09 -0300

    first commit

---
+++ src/recados/recados.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('recados')
+export class RecadosController {
+
+    @Get()
+    findAll(): string {
+        return 'This action returns all recados';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id : string) : string {
+        return `This action returns a recado with id ${id}`;
+    }
+
+@Post()
+create(@Body() body: any): string {
+    return `This action creates a new recado with title ${body.title} and content ${body.content}`;
+}
+}
