cecba96863eb519d1b24b6534042a93e459816b4
Gabriel Salviete <gsalviete@gmail.com>
2025-11-25 15:36:17 -0300

    entities

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,20 +1,22 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { RecadosService } from './recados.service';
 
 @Controller('recados')
 export class RecadosController {
 
-    @Get()
-    findAll(): string {
-        return 'This action returns all recados';
-    }
+  constructor(private readonly recadosService: RecadosService) {}
+  @Get()
+  findAll(): string {
+    return 'This action returns all recados';
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id : string) : string {
-        return `This action returns a recado with id ${id}`;
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a recado with id ${id}`;
+  }
 
-@Post()
-create(@Body() body: any): string {
+  @Post()
+  create(@Body() body: any): string {
     return `This action creates a new recado with title ${body.title} and content ${body.content}`;
-}
+  }
 }
