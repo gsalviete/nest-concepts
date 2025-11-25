44e4b025008a82654015a24783272c6398685d33
Gabriel Salviete <gsalviete@gmail.com>
2025-11-25 16:00:48 -0300

    class-validator

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateRecadoDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @MinLength(10)
   readonly texto?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @MinLength(10)
   readonly de?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @MinLength(10)
   readonly para?: string;
 }
--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -1,12 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRecadoDto } from './dto/create-recado.dto';
 
 @Injectable()
 export class RecadosService {
-  
-    async create(createRecadoDto: CreateRecadoDto): Promise<string> {
-        // Here you would typically handle the creation logic,
-        // such as saving the recado to a database.
-        return await 'This action adds a new recado';
-    }
+  throwNotFoundError() {
+    throw new NotFoundException('Recado not found');
+  }
+
+  findAll(): string {
+    return 'This action returns all recados';
+  }
+
+  async create(createRecadoDto: CreateRecadoDto): Promise<string> {
+    // Here you would typically handle the creation logic,
+    // such as saving the recado to a database.
+    return await 'This action adds a new recado';
+  }
 }
