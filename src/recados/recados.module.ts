8f85fe9f541a1dc248047ae78f24250d7fcf1a5c
Gabriel Salviete <gsalviete@gmail.com>
2025-11-29 15:09:43 -0300

    fix

--- src/recados/recados.module.ts
+++ src/recados/recados.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RecadosController } from './recados.controller';
 import { RecadosService } from './recados.service';
+import { Recado } from './entities/recado.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Recado])
+  ],
   controllers: [RecadosController],
   providers: [RecadosService],
 })
