cb96c66d469768472cccfeed1a708ad4c61b4473
Gabriel Salviete <gsalviete@gmail.com>
2025-11-29 17:36:36 -0300

    enitity config

--- src/recados/entities/recado.entity.ts
+++ src/recados/entities/recado.entity.ts
@@ -1,8 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Recado {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   texto: string;
+  
+  @Column() 
   de: string;
+  
+  @Column()
   para: string;
+  
+  @Column()
   lido: boolean = false;
+  
+  @Column()
   data: Date = new Date();
 }
