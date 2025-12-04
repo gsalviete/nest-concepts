7da19148465f2176d86cfb46cc4ae3793d572ba8
Gabriel Salviete <gsalviete@gmail.com>
2025-12-03 21:26:56 -0300

    config dto

--- src/pessoas/dto/create-pessoa.dto.ts
+++ src/pessoas/dto/create-pessoa.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePessoaDto {}
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreatePessoaDto {
+@IsString()
+@IsNotEmpty()
+nome: string;
+
+@IsNumber()
+@IsNotEmpty()
+idade: number;
+}
