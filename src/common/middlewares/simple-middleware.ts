88d50ef00c3440ad009c91faa1c5740ab688b0f1
Gabriel Salviete <gsalviete@gmail.com>
2026-01-12 14:36:53 -0300

    more middlewares

--- src/common/middlewares/simple-middleware.ts
+++ src/common/middlewares/simple-middleware.ts
@@ -5,8 +5,20 @@ export class SimpleMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
         console.log('simple middleware says: hello!')
 
-        return res.status(404).send({
-            message: 'Not Found'
-        })
+        res.setHeader('Cabecalho', 'do middleware')
+        const authorization = req.headers?.authorization;
+
+        if (authorization){
+            req['user'] = {
+                nome: 'Gabriel',
+                sobrenome: 'Salviete'
+            }
+        }
+
+        next();
+
+        res.on('finish', ()=>{
+            console.log('finish this middleware')
+        } )
     }
 }
